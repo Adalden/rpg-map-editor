b79346a1928a3cfd6c10ae2f4acb3e57fc4db778
numso <dosmun@gmail.com>
2013-08-10 16:18:31 -0600

    have working editor

--- client/js/app/ctrl/main.js
+++ client/js/app/ctrl/main.js
@@ -5,6 +5,19 @@ angular.module('editor').controller('mainCtrl',
 
     $scope.title = 'untitled map';
 
+    $scope.tileSize = 48;
+    $scope.mapWidth = 22;
+    $scope.mapHeight = 14;
+
+    var map = [];
+    for (var i = 0; i < $scope.mapWidth; ++i) {
+      var arr = [];
+      for (var j = 0; j < $scope.mapHeight; ++j) {
+        arr.push(9);
+      }
+      map.push(arr);
+    }
+
     $scope.menu = [
       { label: 'Menu' },
       { label: 'Edit' }
@@ -15,8 +28,6 @@ angular.module('editor').controller('mainCtrl',
       dialogFade: true
     };
 
-    $scope.mapWidth = 1500;
-    $scope.mapHeight = 1500;
 
     $scope.newMap = function (name, width, height) {
       $scope.title = name;
@@ -24,5 +35,79 @@ angular.module('editor').controller('mainCtrl',
       $scope.mapHeight = height;
       $scope.showModal = false;
     };
+
+
+    $scope.curTileChoice = 0;
+
+    $scope.tileTotal = 96;
+
+    $scope.getNumber = function (num) {
+      var arr = [];
+      for (var i = 0; i < num; ++i) {
+        arr.push(i);
+      }
+      return arr;
+    };
+
+    $scope.getStyle = function (num) {
+      var url = 'img/tiles.jpg';
+      var cols = 8;
+      var margin = 3;
+
+      var tsize = $scope.tileSize + margin;
+
+      var x = num % cols;
+      x *= tsize;
+
+      var y = Math.floor(num / cols);
+      y *= tsize;
+
+      var str = 'background:url(' + url + ') ' + -x + 'px ' + -y + 'px;';
+      str += 'width:' + $scope.tileSize + 'px;';
+      str += 'height:' + $scope.tileSize + 'px;';
+
+      return str;
+    };
+
+
+    $scope.clickMap = function (e) {
+      var x = e.offsetX;
+      var y = e.offsetY;
+
+      var tileX = Math.floor(x / $scope.tileSize);
+      var tileY = Math.floor(y / $scope.tileSize);
+
+      map[tileX][tileY] = $scope.curTileChoice;
+
+      redrawCanvas();
+    };
+
+    function redrawCanvas() {
+      var canvas = document.getElementById('map');
+      var ctx = canvas.getContext('2d');
+      var ts = $scope.tileSize;
+
+      for (var i = 0; i < map.length; ++i) {
+        for (var j = 0; j < map[i].length; ++j) {
+          drawImage(ctx, map[i][j], i, j, ts);
+        }
+      }
+    }
+
+    function drawImage(ctx, tileNum, x, y, ts) {
+      var cols = 8;
+      var margin = 3;
+      var sx = tileNum % cols;
+      var sy = Math.floor(tileNum / cols);
+      ctx.drawImage(img, sx * (ts + margin), sy * (ts + margin), ts, ts, x * ts, y * ts, ts, ts);
+    }
+
+    var img = new Image();
+    img.onload = function () {
+      setTimeout(redrawCanvas);
+    };
+    img.src = 'img/tiles.jpg';
+
+
   }
 );
