ef44bc9412b506bd5cb869948e9f866965a5f677
numso <dosmun@gmail.com>
2013-08-13 19:48:27 -0600

    added grid toggle button

--- client/js/app/ctrl/header.js
+++ client/js/app/ctrl/header.js
@@ -1,13 +1,25 @@
 /* global angular */
 angular.module('editor').controller('headerCtrl',
-  function ($scope, $rootScope, $state, map, menu) {
+  function ($scope, $rootScope, $state, map, menu, tools) {
     'use strict';
 
     $scope.map = map;
     $scope.menu = menu;
 
     $scope.goToState = function (state) {
-      $state.transitionTo(state);
+      if (state.isCommand) {
+        funcs[state.id]();
+        return;
+      }
+      $state.transitionTo(state.state);
+    };
+
+    function toggleGrid() {
+      tools.showGrid = !tools.showGrid;
+    }
+
+    var funcs = {
+      grid: toggleGrid
     };
   }
 );
